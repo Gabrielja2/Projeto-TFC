bfab584976d9774636340222ad1aa8b2da0e55eb
Gabriel <gabrielja2antunes@gmail.com>
2022-10-21 19:52:09 -0300

    feat: requisito 18 finalizado

---
+++ app/backend/src/database/models/Matche.ts
@@ -0,0 +1,47 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class Matche extends Model {
+  id!: number;
+  homeTeam!: number;
+  homeTeamGoals!: number;
+  awayTeam!: number;
+  awayTeamGoals!: number;
+  inProgress!: number;
+}
+
+Matche.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: INTEGER,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+  underscored: true,
+});
+
+export default Matche;
