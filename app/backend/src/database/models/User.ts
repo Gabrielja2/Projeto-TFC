eca84787d20b8421e34154ae71f1277c51b9520f
Gabriel <gabrielja2antunes@gmail.com>
2022-10-21 17:34:45 -0300

    requisito 01 finalizado

---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,41 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  id!: number;
+  email!: string;
+  name!: string;
+  password!: string;
+  role!: string;
+}
+
+User.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  email: {
+    type: STRING,
+    allowNull: false,
+  },
+  name: {
+    type: STRING,
+    allowNull: false,
+  },
+  password: {
+    type: STRING,
+    allowNull: false,
+  },
+  role: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'users',
+  timestamps: false,
+});
+
+export default User;
