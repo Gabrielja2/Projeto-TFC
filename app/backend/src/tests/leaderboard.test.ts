37aa2b75bf0f236e1853a2348abb53398cc27fc6
Gabriel <gabrielja2antunes@gmail.com>
2022-10-28 13:12:56 -0300

    chore: testes da rota leaderboard/away

--- app/backend/src/tests/leaderboard.test.ts
+++ app/backend/src/tests/leaderboard.test.ts
@@ -5,7 +5,7 @@ import chaiHttp = require('chai-http');
 import { app } from '../app';
 import { Response } from 'superagent';
 import sequelize from '../database/models/index';
-import { leaderboardHome } from './mocks/leaderboardMock';
+import { leaderboardAway, leaderboardHome } from './mocks/leaderboardMock';
 
 
 chai.use(chaiHttp);
@@ -25,6 +25,16 @@ describe('Testando a rota /leaderboard', () => {
 
     expect(response.status).to.be.equal(200);
     expect(response.body).to.be.deep.equal(leaderboardHome);
+  })
+
+  it('Verifica se retorna leaderboardAway corretamente e o status correto', async() => {
+    sinon.stub(sequelize, 'query').resolves([leaderboardAway] as any)
+    const response = await chai
+      .request(app)
+      .get('/leaderboard/away');
+
+    expect(response.status).to.be.equal(200);
+    expect(response.body).to.be.deep.equal(leaderboardAway);
   })
 
 });
